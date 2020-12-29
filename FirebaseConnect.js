c7060cbba1835ed3aecc22f63aa1f24fe95922d0
Maitra Patel <maitrapatel@yahoo.com>
2020-12-28 22:02:15 -0500

    firebaseConnect_trial2

---
+++ FirebaseConnect.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { View, Text, Button } from 'react-native';
+
+import firestore from  "@react-native-firestore/firestore";
+
+
+
+class FirebaseConnect extends Component {
+    constructor(props){
+        super(props);
+        this.getUser();
+    }
+
+    getUser = async () => {
+        const UserDocument = await firestore().collection("Users").doc('HiXymxqloChH0olghN3k').get()
+        console.log(UserDocument)
+    }
+
+    render() {
+        return (
+            <View>
+                <Text> Hello World </Text>    
+            </View>
+        );
+    }
+}
+
+export default FirebaseConnect;
+
