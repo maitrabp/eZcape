922f876e66c7274b8facaa00774039b17cf5f119
Maitra Patel <maitrapatel@yahoo.com>
2021-01-01 16:46:47 -0500

    Updated_Validation_Errors..

--- Screens/Login.js
+++ Screens/Login.js
@@ -16,36 +16,83 @@ export default function Login({navigation}) {
     
     const login = () => {
         console.log(email);
-        if (emailErrorFree && passwordErrorFree)
+        if (emailError === '' && passwordError === '')
         {
             firebase.auth().signInWithEmailAndPassword(email, password)
             .then((res) => {
                 console.log(res)
                 console.log('User logged-in successfully!')
-                navigation.navigate('Home');
+                navigation.navigate('Home')
             })
-        }
-        
+            .catch((error) => {
+                if (error.code === "auth/invalid-email")
+                {
+                    setEmailError(error.message)
+                }
+                else if (error.code === "auth/user-not-found")
+                {
+                    alert(error.message)
+                }
+                else if (error.code === "auth/invalid-password")
+                {
+                    setPasswordError(error.message)
+                }
+                else {
+                    console.error(error)
+                }
+            })
+        }  
     }
 
     const redirectSignUp = () => {
         navigation.navigate('SignUp');
     }
 
-    const emailHandler = (typedText) => {
+    const emailOBvalidation = () => {
+        
+        if (email.length < 2)
+        {
+            setEmailError("Email must not be empty!");
+            // setEmailErrorFree(false);
+        } else {
+            setEmailError("");
+            // setEmailErrorFree(true);
+        }
+    }
+    const emailOCTvalidation = (typedText) => {
         
         if (typedText === '')
         {
             setEmailError("Email must not be empty!");
-            setEmailErrorFree(false);
+            // setEmailErrorFree(false);
         } else {
             setEmailError("");
             setemail(typedText);
-            setEmailErrorFree(true);
+            // setEmailErrorFree(true);
+        } 
+        //more if and else conditions to validate other things..
+    }
+    const passwordOBvalidation = () => {
+        //1. Empty Check
+        //2. Maximum length check
+        //3. Minimum length check
+        //4. Strength check (combinations of char and num.)
+        
+        if (password.length < 2)
+        {
+            setPasswordError("Password must not be empty!");
+            // setPasswordErrorFree(false);
+        }
+        else if (password.length < 8)
+        {
+            setPasswordError("Password must be atleast 8 characters in length");
+        } else {
+            setPasswordError("");
+            // setPasswordErrorFree(true);
         }
     }
 
-    const passwordHandler = (typedText) => {
+    const passwordOCTvalidation = (typedText) => {
         //1. Empty Check
         //2. Maximum length check
         //3. Minimum length check
@@ -54,11 +101,15 @@ export default function Login({navigation}) {
         if (typedText == '')
         {
             setPasswordError("Password must not be empty!");
-            setPasswordErrorFree(false);
+            // setPasswordErrorFree(false);
+        }
+        else if (typedText.length < 8)
+        {
+            setPasswordError("Password must be atleast 8 characters in length");
         } else {
             setPasswordError("");
             setPassword(typedText);
-            setPasswordErrorFree(true);
+            // setPasswordErrorFree(true);
         }
     }
     
@@ -70,14 +121,16 @@ export default function Login({navigation}) {
                 placeholder="Email" 
                 defaultValue={email}
                 error={emailError}
-                onChangeText={emailHandler}
+                onBlur = {emailOBvalidation}
+                onChangeText={emailOCTvalidation}
             />
             <EzTextInput 
                 placeholder="Password" 
                 defaultValue={password}
                 secureTextEntry={true}
                 error={passwordError}
-                onChangeText={passwordHandler}
+                onBlur = {passwordOBvalidation}
+                onChangeText={passwordOCTvalidation}
             />
             <EzButton onPress={login} title={"Login"} />
             <EzButton onPress={redirectSignUp} title={"New User? Sign Up"} /> 
