b21a463a92869126c7ab450221e556feca3f2e95
nisargdpatel <46580820+nisargdpatel@users.noreply.github.com>
2021-02-11 17:39:58 -0500

    Tried to resolve arrays issue

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,45 +1,117 @@
-import React, {useState} from 'react'
+import React, {Component, useState, useEffect} from 'react'
 import { View, Text, FlatList, Button, ImageBackground, StyleSheet } from 'react-native'
 import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
 import EzTripCard from '../Components/EzTripCard'
 import firebase, {db} from '../Firebase/firebaseConfig';
 import * as Animatable from 'react-native-animatable';
+import AppLoading from 'expo-app-loading';
 import EzButton from '../Components/EzButton';
 import { concat } from 'react-native-reanimated';
+import { render } from 'react-dom';
+
+    
 
 export default function Home({navigation}) {
 
-    const managementDocs = [];
-    const tripDocs = [];
+        // const managementDocs = [];
+        // const tripDocs = [];
 
-    var user = firebase.auth().currentUser;
-    var userId = user.uid;
-    const usersCollection = db.collection('Users');
-    const managementCollection = db.collection('Management');
-    const tripsCollection = db.collection('Trips');
-    
-    
-    async function getManagementDocs() {
-        // console.log(tripsCollection.doc('2rEkbJAwWnfzK8wUk6wQ'));
-        const snapshot = await managementCollection.where('UserID', '==', userId).get();
-        snapshot.forEach(doc => {
-            managementDocs.push(doc.data().TripID);
+        const [managementDocs, setManagementDocs] = useState(["dummy"]);
+        const [tripDocs, setTripDocs] = useState([{}]);
+
+        const [dataFetchDone, setDataFetchDone] = useState(false);
+
+        const user = firebase.auth().currentUser;
+        const userId = user.uid;
+        const usersCollection = db.collection('Users');
+        const managementCollection = db.collection('Management');
+        const tripsCollection = db.collection('Trips');
+
+        function ehh(){
+            console.log(tripDocs);
+        }
+        
+        useEffect(() => {
+            await getManagementDocs();
             
-        });
-        getTrips();
-        
-        // console.log(managementDocs);
-    }
+        }, [])
 
-
-    async function getTrips() {
-        managementDocs.forEach(tripId => {
-                tripsCollection.doc(tripId).get().then(data => {
-                console.log(data.data());
+        async function getManagementDocs() {
+            // console.log(tripsCollection.doc('2rEkbJAwWnfzK8wUk6wQ'));
+            // const managementCollection = db.collection('Management');
+            const snapshot = await managementCollection.where('UserID', '==', user.uid).get();
+            snapshot.forEach(doc => {
+               // managementDocs.push(doc.data().TripID);
+                console.log(doc.data().TripID)
+                setManagementDocs(oldData => [...oldData, doc.data().TripID]);
+                console.log("ManagementDocs:", managementDocs);
             });
-        })
+            console.log("ManagementDocs:", managementDocs);
+            getTrips();
+            
+            // console.log(managementDocs);
+        }
+
+
+        async function getTrips() {
+            // const tripsCollection = db.collection('Trips');
+            managementDocs.forEach(tripId => {
+                    tripsCollection.doc(tripId).get().then(data => {
+                    // tripDocs.push(data.data());
+                    console.log(data.data())
+                    setTripDocs(oldData => [...oldData, data.data()]);
+                    // console.log(tripDocs);
+                });
+            })
+            console.log("Trip Docs:", tripDocs);
+        }
+
+        const renderItem = ({ item }) => (
+            <EzTripCard 
+                tripName={item.tripName}
+                numMembers={item.numMembers}
+                balance={item.balance}>
+                {/* <Text>{item.tripName}</Text> */}
+            </EzTripCard>
+            
+        );
+
+        // if (dataFetchDone)
+        // {
+            console.log(tripDocs);
+            return (
+                <ImageBackground source = {require('../Assets/loginBackground.jpg')} style={{flex: 1}}>
+                    <Animatable.View animation="fadeInDown" duration={1000} style={styles.container}>
+                    <ScrollView>
+                        <FlatList 
+                            data={tripDocs}
+                            keyExtractor={item => item.tripName}
+                            renderItem={renderItem}
+                        />
+                    </ScrollView>
+                        {/* <EzButton onPress={getManagementDocs} value="Click Me">
+                        </EzButton> */}
+
+                    </Animatable.View>
+                </ImageBackground>
+            )
+        // } else {
+        //     return (
+        //         <AppLoading
+        //             startAsync={useEffect()}
+        //             onFinish={() => {setDataFetchDone(true)}}
+        //             onError={console.warn}
+        //         />
+        //     )
+        // }
         
+    
+
+
+    
     }
+
+    
 
     
     // this.getUser();
@@ -121,33 +193,10 @@ export default function Home({navigation}) {
         
     ]
 
-    const renderItem = ({ item }) => (
-        <EzTripCard 
-            tripName={item.tripName}
-            numMembers={item.numMembers}
-            balance={item.balance}>
-            {/* <Text>{item.tripName}</Text> */}
-        </EzTripCard>
-        
-      );
+    
 
-    return (
-        <ImageBackground source = {require('../Assets/loginBackground.jpg')} style={{flex: 1}}>
-            <Animatable.View animation="fadeInDown" duration={1000} style={styles.container}>
-            {/* <ScrollView> */}
-                {/* <FlatList 
-                    data={DATA}
-                    // keyExtractor={item => item.id}
-                    renderItem={renderItem}
-                /> */}
-            {/* </ScrollView> */}
-                <EzButton onPress={getManagementDocs} value="Click Me">
-                </EzButton>
-
-            </Animatable.View>
-        </ImageBackground>
-    )
-}
+    
+// }
 
 //Styles for Home Page
 const styles = StyleSheet.create({
